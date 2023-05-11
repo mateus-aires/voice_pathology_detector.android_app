a251ed83747a0dbee1bb85c7532f58a1ff111cae
Mateus Aires <mateus.aires@ccc.ufcg.edu.br>
2023-05-11 15:42:36 -0300

    Refactored main code

--- VoicePathologyDetector/app/src/main/java/com/airesapps/util/Constants.java
+++ VoicePathologyDetector/app/src/main/java/com/airesapps/util/Constants.java
@@ -1,6 +1,17 @@
 package com.airesapps.util;
 
 public class Constants {
+    // MainActivity constants
+    public static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    public static final int STATE_LOGO = -1;
+    public static final int STATE_INSTRUCTION = 0;
+    public static final int STATE_RECORDING = 1;
+    public static final int STATE_PROCESSING = 2;
+    public static final int STATE_RESULT_SUCCESS = 3;
+    public static final int STATE_RESULT_ERROR = 4;
+    public static final String CHRONOMETER_FORMAT = "mm:ss";
+
+    // Messages
     public static final String TRUE_PREDICTION_MESSAGE = "Você está apresentando sintomas de disfonia";
     public static final String FALSE_PREDICTION_MESSAGE = "Você parece estar saudável";
     public static final String PRESENT_PROBA_FORMAT = "Probabilidade de estar doente: %d";
